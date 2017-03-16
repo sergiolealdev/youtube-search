42cf4f838ba3ef89a7b5aa355b3d8f6f2f9f5aea
Sergio Leal <sergio.leal@hse.ie>
2017-03-16 17:39:59 +0000

    modifying services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,27 @@ import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
+import {YOUTUBE_API_KEY, YOUTUBE_API_URL, YoutubeServiceService} from './youtube-service.service';
+import {SearchResult} from './search-result';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBoxComponent,
     SearchResultComponent,
-    YoutubeSearchComponent
+    YoutubeSearchComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    {provide: YoutubeServiceService, useClass: YoutubeServiceService},
+    {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
+    {provide: YOUTUBE_API_URL, useValue: YOUTUBE_API_URL}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {SearchResult} from '../search-result';
 
 @Component({
@@ -8,7 +8,7 @@ import {SearchResult} from '../search-result';
 })
 export class SearchResultComponent implements OnInit {
 
-  result: SearchResult;
+  @Input result: SearchResult;
 
   constructor() { }
 
